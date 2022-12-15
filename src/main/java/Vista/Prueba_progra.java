db695b77c357033ae2bbe3c40c37fb0bc684f8d4
Manuel <Manuel@DESKTOP-HVAM6IP>
2022-12-15 09:57:14 -0500

    probando

---
+++ src/main/java/Vista/Prueba_progra.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Project/Maven2/JavaApp/src/main/java/${packagePath}/${mainClassName}.java to edit this template
+ */
+
+package Vista;
+
+/**
+ *
+ * @author Manuel
+ */
+public class Prueba_progra {
+
+    public static void main(String[] args) {
+        Ventana1 ventana1=new Ventana1();
+        ventana1.setVisible(true);
+    }
+}
